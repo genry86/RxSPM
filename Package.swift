9487acaa98b7146effba2746f334a7e1895a8509
Genry <genry@tonsser.com>
2020-09-30 17:24:04 +0300

    Increase version v9

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RxSPM",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v9)
     ],
     products: [
         .library(name: "Rx", targets: ["Rx"]),
